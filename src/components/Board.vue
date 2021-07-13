b024c0e53e60ce86725204cc4170ca3dd36dc613
mizukenw <kentamizu75@gmail.com>
2021-07-13 11:13:43 +0900

    初めてのvue

---
+++ src/components/Board.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <header>my Trello</header>
+    <main>
+      <p class="info-line">All: 0 task</p>
+    </main>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
