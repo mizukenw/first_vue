c7f868b5fff7d32bea01ffad3df13a0c3177df13
mizukenw <kentamizu75@gmail.com>
2021-07-15 13:36:46 +0900

    ドラッグ&ドロップ機能追加

--- src/components/Board.vue
+++ src/components/Board.vue
@@ -3,27 +3,30 @@
     <header>my Trello</header>
     <main>
       <p class="info-line">All: {{ totalCardCount }} tasks</p>
-      <div class="list-index">
+      <draggable :list="lists" @end="movingList" class="list-index">
         <list
           v-for="(item, index) in lists"
           :key="item.id"
           :title="item.title"
           :cards="item.cards"
           :listIndex="index"
+          @change="movingCard"
         />
         <list-add />
-      </div>
+      </draggable>
     </main>
   </div>
 </template>
 
 <script>
+import draggable from "vuedraggable";
 import ListAdd from "./ListAdd";
 import List from "./List";
 import { mapState } from "vuex";
 
 export default {
   components: {
+    draggable,
     ListAdd,
     List,
   },
@@ -31,6 +34,14 @@ export default {
     ...mapState(["lists"]),
     totalCardCount() {
       return this.$store.getters.totalCardCount;
+    },
+  },
+  methods: {
+    movingCard: function () {
+      this.$store.dispatch("updateList", { lists: this.lists });
+    },
+    movingList: function () {
+      this.$store.dispatch("updateList", { lists: this.lists });
     },
   },
 };
